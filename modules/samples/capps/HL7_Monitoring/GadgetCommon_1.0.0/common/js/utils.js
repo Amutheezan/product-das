60e306193c62d42895d67edb3953d380d9291b37
Amutheezan <amux93@yahoo.com>
2016-11-21 17:22:13 +0530

    added capp with search and summary completed

---
+++ modules/samples/capps/HL7_Monitoring/GadgetCommon_1.0.0/common/js/utils.js
@@ -0,0 +1,29 @@
+var CONTEXT = "/portal/apis/data";
+
+function getQueryString() {
+    var queryStringKeyValue = window.parent.location.search.replace('?', '').split('&');
+    var qsJsonObject = {};
+    if (queryStringKeyValue != '') {
+        for (i = 0; i < queryStringKeyValue.length; i++) {
+            qsJsonObject[queryStringKeyValue[i].split('=')[0]] = queryStringKeyValue[i].split('=')[1];
+        }
+    }
+    return qsJsonObject;
+}
+
+function fetchData(params, callback, error) {
+    $.ajax({
+        url: "/portal/apis/hl7?msgType=" + params.msgType + "&timeFrom=" + params.timeFrom + "&timeTo=" + params.timeTo,
+        type: "GET",
+        success: function(data) {
+            callback(data);
+        },
+        error: function(msg) {
+            error(msg);
+        }
+    });
+
+
+
+
+}
---
+++ modules/samples/capps/HL7_Monitoring/GadgetSearchPage_1.0.0/searchpg/js/gadgetconf.js
@@ -0,0 +1,11 @@
+var configs = [{
+        name: ROLE_SEARCH,
+        type: 2,
+        columns: [
+            { name: "timestamp", label: "timestamp", type: "linear" },
+            { name: "correlation_activity_id", label: "correlation_activity_id", type: "linear" },
+            { name: "meta_host", label: "meta_host", type: "linear" },
+            { name: "message_direction", label: "message_direction", type: "linear" }
+        ],
+    }
+];

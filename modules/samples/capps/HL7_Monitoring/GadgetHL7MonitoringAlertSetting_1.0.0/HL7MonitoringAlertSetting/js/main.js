c3783fd168f7b1634cee125206eee2b7940fb7ed
amutheezan <amux93@yahoo.com>
2016-12-11 05:29:10 +0530

    improved execution plan and publishers

--- modules/samples/capps/HL7_Monitoring/GadgetHL7MonitoringAlertSetting_1.0.0/HL7MonitoringAlertSetting/js/main.js
+++ modules/samples/capps/HL7_Monitoring/GadgetHL7MonitoringAlertSetting_1.0.0/HL7MonitoringAlertSetting/js/main.js
@@ -1,21 +1,26 @@
-var TYPE_ALERT_LIMIT_UPDATE=31;
-var TYPE_GET_CURRENT_EXECUTION_PLAN=32
-var DEFAULT_DISEASE_ALERT_LIMIT=1000;
-var DEFAULT_WAITINGTIME_ALERT_LIMIT=1000;
+var TYPE_UPDATE=31;
+var TYPE_GET_CURRENT_SETTING=32;
+var DEFAULT_ALERT_LIMIT=1000;
+
 var url= "/portal/store/carbon.super/fs/gadget/HL7MonitoringGadgetCommon/jaggery-api/hl7.jag";
 var type=null;
-var diseaseAlertLimit=0;
-var waitTimeAlertLimit=0;
+var AlertLimit=0;
+var AlertType,EmailAddress,PhoneNo=null;
 
 $(function(){
-type=TYPE_GET_CURRENT_EXECUTION_PLAN;
+type=TYPE_GET_CURRENT_SETTING;
 $.ajax({
     url: url +"?type=" + type,
     type: "GET",
     success: function(data) {
     $("#diseaseAlert").val(data.message[0]["diseaseAlertLimit"]);
     $("#waitTimeAlert").val(data.message[0]["waitTimeAlertLimit"]/1000);
-    $("#update").html(data.message[0]["initialize"]);
+    $("#daEmail").val(data.message[0]["daEmail"]);
+    $("#daPhoneNo").val(data.message[0]["daPhoneNo"]);
+    $("#wtaEmail").val(data.message[0]["wtaEmail"]);
+    $("#wtaPhoneNo").val(data.message[0]["wtaPhoneNo"]);
+    $("#waitTimeupdate").html(data.message[0]["dainitialize"]);
+    $("#diseaseupdate").html(data.message[0]["wtainitialize"]);
 
     },
     error: function(data){
@@ -23,65 +28,79 @@ $.ajax({
 
 });
 
-$("#update").click(function(){
-  diseaseAlertLimit=$('#diseaseAlert').val()
-  waitTimeAlertLimit=$('#waitTimeAlert').val();
-  createExecutionPlan(diseaseAlertLimit,waitTimeAlertLimit);
-});
-
-
-});
-
-function createExecutionPlan(diseaseAlertLimit,waitTimeAlertLimit){
-  type=TYPE_ALERT_LIMIT_UPDATE;
-  if(isNull(diseaseAlertLimit) || diseaseAlertLimit <= 0){
-    diseaseAlertLimit=DEFAULT_DISEASE_ALERT_LIMIT;
-    $("#diseaseAlert").val(DEFAULT_DISEASE_ALERT_LIMIT);
-  }else {
-  if(diseaseAlertLimit === parseInt(diseaseAlertLimit, 10)){
-   diseaseAlertLimit=DEFAULT_DISEASE_ALERT_LIMIT;
-    $("#diseaseAlert").val(DEFAULT_DISEASE_ALERT_LIMIT);
-  } else{
-     
-  }
-  }
-  if(isNull(waitTimeAlertLimit || waitTimeAlertLimit <= 0)){
-    waitTimeAlertLimit=DEFAULT_WAITINGTIME_ALERT_LIMIT;
-    $("#waitTimeAlert").val(DEFAULT_WAITINGTIME_ALERT_LIMIT);
-   }else {
-  if(waitTimeAlertLimit === parseInt(waitTimeAlertLimit, 10)){
-        waitTimeAlertLimit=DEFAULT_DISEASE_ALERT_LIMIT;
-    $("#waitTimeAlert").val(DEFAULT_WAITINGTIME_ALERT_LIMIT);
-  } else{
-
-  }
-  }
-    var params={ type:type,
-    diseaseAlertLimit:diseaseAlertLimit,
-          waitTimeAlertLimit:waitTimeAlertLimit
-        }
-      $.ajax({
-          url: url +"?type=" + params.type + "&diseaseAlertLimit=" + params.diseaseAlertLimit + "&waitTimeAlertLimit=" + params.waitTimeAlertLimit,
-          type: "GET",
- 	 success: function(data) {
-    $("#update").html(data.message[0]["initialize"]);
-
-    },
-    error: function(data){
+  $("#diseaseupdate").click(function(){
+    type=TYPE_UPDATE;
+    AlertType="disease";
+    AlertLimit=$("#diseaseAlert").val();
+    EmailAddress=$("#diseaseEmail").val();
+    PhoneNo=$("#diseasePhoneNo").val();
+    if(isNull(AlertLimit) || AlertLimit <= 0){
+      AlertLimit=DEFAULT_ALERT_LIMIT;
+      $("#diseaseAlert").val(DEFAULT_ALERT_LIMIT);
+    }else {
+    if(AlertLimit === parseInt(AlertLimit, 10)){
+     AlertLimit=DEFAULT_ALERT_LIMIT;
+      $("#diseaseAlert").val(DEFAULT_ALERT_LIMIT);
+    } else{
     }
+    }
+      var params={ type:type,
+        AlertType:AlertType,
+        AlertLimit:AlertLimit,
+        EmailAddress:EmailAddress,
+        PhoneNo:PhoneNo
+          }
+        $.ajax({
+            url: url +"?type=" + params.type + "&AlertType=" + params.AlertType + "&AlertLimit=" + params.AlertLimit + "&EmailAddress=" + params.EmailAddress + "&PhoneNo=" + params.PhoneNo,
+            type: "GET",
+             success: function(data) {
+              $("#diseaseupdate").html(data.message[0]["initialize"]);
+              },
+        });
+  });
 
-          
-      });
+  $("#waitTimeupdate").click(function(){
+    type=TYPE_UPDATE;
+    AlertType="waitTime";
+    AlertLimit=$("#waitTimeAlert").val();
+    EmailAddress=$("#waitTimeEmail").val();
+    PhoneNo=$("#waitTimePhoneNo").val();
+    if(isNull(AlertLimit) || AlertLimit <= 0){
+      AlertLimit=DEFAULT_ALERT_LIMIT;
+      $("#waitTimeAlert").val(DEFAULT_ALERT_LIMIT);
+    }else {
+    if(AlertLimit === parseInt(AlertLimit, 10)){
+     AlertLimit=DEFAULT_ALERT_LIMIT;
+      $("#waitTimeAlert").val(DEFAULT_ALERT_LIMIT);
+    } else{
+    }
+    }
+      var params={ type:type,
+        AlertType:AlertType,
+        AlertLimit:AlertLimit,
+        EmailAddress:EmailAddress,
+        PhoneNo:PhoneNo
+          }
+        $.ajax({
+            url: url +"?type=" + params.type + "&AlertType=" + params.AlertType + "&AlertLimit=" + params.AlertLimit + "&EmailAddress=" + params.EmailAddress + "&PhoneNo=" + params.PhoneNo,
+            type: "GET",
+             success: function(data) {
+              $("#waitTimeupdate").html(data.message[0]["initialize"]);
+              },
+        });
+  });
 
-}
 
-function setValues(data){
-  console.log(data+" is the data");
-}
+
+
+
+
+});
+
 
 function isNull(value){
   var isNull=false;
-  if(typeof value === "undefined" || value === null || value === "" ){
+  if(typeof value === "undefined" || value === null || value === ""  ){
     isNull=true;
   }
   return isNull;
